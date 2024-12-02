14c7cbd302526509ab5be2bd464ea3685452e55e
psm0419 <sm8804@naver.com>
2024-12-02 10:12:49 +0900

    폴더 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import './App.css';
+import ExerciseMain from './pentagon/ExerciseMain';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DetailPage from './pentagon/DetailPage';
+
 
 function App() {
-  return (
-    <div>
 
-    </div>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ExerciseMain />} />
+        <Route path="/details" element={<DetailPage />} />
+        <Route path="*" element={<div>경로 오류</div>} />
+      </Routes>
+    </Router>
   );
+
+
 }
 
 export default App;
